3f2770174884612ec6bacde37c34a0fa3ce0c0fa
Home <home@Homes-MBP.lan>
2024-09-14 00:20:46 -0400

    N

---
+++ tests/unit/tesing_app.py
@@ -0,0 +1,16 @@
+import pytest
+import requests
+
+@pytest.fixture
+def base_url():
+    return "http://3.141.40.42"
+
+def test_get_request(base_url):
+    response = requests.get(f"{base_url}/explore")
+    assert response.status_code == 200
+
+# def test_post_request(base_url):
+#     payload = {"title": "foo", "body": "bar", "userId": 1}
+#     response = requests.post(f"{base_url}/posts", json=payload)
+#     assert response.status_code == 201
+#     assert response.json()['title'] == "foo"
