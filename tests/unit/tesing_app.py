a3b34c5f2b9b566c1d32c1c4ecbfc827d51990c4
Home <home@Homes-MBP.lan>
2024-09-14 00:23:31 -0400

    N

--- tests/unit/tesing_app.py
+++ tests/unit/tesing_app.py
@@ -6,7 +6,7 @@ def base_url():
     return "http://3.141.40.42"
 
 def test_get_request(base_url):
-    response = requests.get(f"{base_url}/explore")
+    response = requests.get(f"{base_url}/explores")
     assert response.status_code == 200
 
 # def test_post_request(base_url):
