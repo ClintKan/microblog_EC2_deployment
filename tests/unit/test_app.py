ec6285608aff1886b785d06bf4451239159b2c50
Home <home@Homes-MBP.lan>
2024-09-14 00:07:24 -0400

    again

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,16 +1,16 @@
 import sys
 import os
 import unittest
-import requests
 
 # Appending the directory path where routes.py is located
 sys.path.append(os.path.abspath("/home/ubuntu/microblog_EC2_deployment/app/main/routes.py"))
 
 # importing routes
 from app.main.routes import explore
+from app.main import routes
 
 def test_explore_page():
-    response = request.get('/explore')
+    response = routes.test_client().get('/explore')
     assertEqual(response.status_code, 200)  # OK
 
 if __name__ == '__main__':
