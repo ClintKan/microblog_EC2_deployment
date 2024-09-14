61f823062246cc8a175a5a04b3b8681335a107e8
Home <home@Homes-MBP.lan>
2024-09-13 23:05:37 -0400

    creating a test file

---
+++ tests/unit/test_app.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import unittest
+
+# Appending the directory path where routes.py is located
+sys.path.append(os.path.abspath("/home/ubuntu/microblog_EC2_deployment/app/main/routes.py"))
+
+# importing routes
+from routes import explore
+
+def test_explore_page():
+    response = request.get('/explore')
+    assertEqual(response.status_code, 200)  # OK
+
+if __name__ == '__main__':
+    unittest.main()
