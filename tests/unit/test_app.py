ceea780d2f56aec5a02e7d10b98656f1066db705
Home <home@Homes-MBP.lan>
2024-09-14 23:28:09 -0400

    Updates

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -5,14 +5,14 @@ import requests
 def base_url():
     return "http://13.58.232.164"
 
-def test_get_request(base_url):
-    response = requests.get(f"{base_url}/")
-    assert response.status_code == 200
+# def test_get_home(base_url):
+#     response = requests.get(f"{base_url}/")
+#     assert response.status_code == 200
 
-def test_get_request(base_url):
-    response = requests.get(f"{base_url}/auth/login")
-    assert response.status_code == 200
+# def test_get_login(base_url):
+#     response = requests.get(f"{base_url}/auth/login")
+#     assert response.status_code == 200
 
-def test_get_request(base_url):
+def test_get_explore(base_url):
     response = requests.get(f"{base_url}/explore")
     assert response.status_code == 200
