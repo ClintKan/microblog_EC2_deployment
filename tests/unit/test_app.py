a30e7a705d4b660c1308d1248557e1b09a1ed0b4
Home <home@Homes-MBP.lan>
2024-09-15 14:11:29 -0400

    day0915_4.5

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -3,7 +3,7 @@ import requests
 
 @pytest.fixture
 def base_url():
-    return "http://3.144.229.235"
+    return "http://3.20.235.84"
 
 def test_get_home(base_url):
      response = requests.get(f"{base_url}/", timeout=3)
