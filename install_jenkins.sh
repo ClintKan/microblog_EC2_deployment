3656a7f63677afad293ef705f2d1bea17c9ff155
Home <home@Homes-MBP.lan>
2024-09-15 15:01:17 -0400

    day0915_5.1

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,7 +1,12 @@
-sudo apt update && sudo apt install fontconfig openjdk-17-jre software-properties-common && sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt install python3.7 python3.7-venv
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install fontconfig openjdk-17-jre software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install python3.7 python3.7-venv -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/nul 
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
